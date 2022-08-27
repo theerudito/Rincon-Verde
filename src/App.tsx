8f6536bc13a6f13ca77c5637ec482ef30f59400a
theerudito <erudito.tv@gmail.com>
2022-08-27 12:58:53 -0500

    v5

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Footer } from "./Componets/Footer/Footer";
 import { Header } from "./Componets/Header/Header";
 import { Main } from "./Componets/Main/Main";
 import "./Componets/scss/styles.scss";
-import florIzq from "../src//Componets/image/floriz.svg";
+
 
 function App() {
   return (
--- src/Componets/Main/Carrousel.tsx
+++ src/Componets/Main/Carrousel.tsx
@@ -1,4 +1,9 @@
-import React from "react";
+import Image1 from "../photosRinconVerde/1.jpg";
+import Image2 from "../photosRinconVerde/2.jpg";
+import Image3 from "../photosRinconVerde/3.jpg";
+import Image4 from "../photosRinconVerde/4.jpg";
+import Image5 from "../photosRinconVerde/5.jpg";
+import Image6 from "../photosRinconVerde/6.jpg";
 
 export const Carrousel = () => {
   return (
@@ -6,54 +11,26 @@ export const Carrousel = () => {
       <i className="fa-solid fa-circle-left" id="left"></i>
       <i className="fa-solid fa-circle-right" id="right"></i>
       <div className="contenedorImage1">
-        <img
-          src="https://placeimg.com/640/480/animals"
-          alt="galeria"
-          className="image1"
-        />
+        <img src={Image1} alt="galeria" className="image1" />
       </div>
       <div className="contenedorImage2">
-        <img
-          src="https://placeimg.com/640/480/arch"
-          alt="galeria"
-          className="image2"
-        />
+        <img src={Image2} alt="galeria" className="image2" />
       </div>
       <div className="contenedorImage3">
-        <img
-          src="https://placeimg.com/640/480/nature"
-          alt="galeria"
-          className="image3"
-        />
+        <img src={Image3} alt="galeria" className="image3" />
       </div>
 
       <div className="contenedorImage5">
-        <img
-          src="https://placeimg.com/640/480/nature"
-          alt="galeria"
-          className="image5"
-        />
+        <img src={Image4} alt="galeria" className="image5" />
       </div>
 
       <div className="contenedorImage6">
-        <img
-          src="https://placeimg.com/640/480/nature"
-          alt="galeria"
-          className="image6"
-        />
+        <img src={Image5} alt="galeria" className="image6" />
       </div>
 
       <div className="contenedorImage4">
-        <img
-          src="https://placeimg.com/640/480/nature"
-          alt="galeria"
-          className="image4"
-        />
+        <img src={Image3} alt="galeria" className="image4" />
       </div>
     </div>
   );
 };
-
-// <img src="https://placeimg.com/640/480/animals" alt="galeria" />
-// <img src="https://placeimg.com/640/480/arch" alt="galeria" />
-// <img src="https://placeimg.com/640/480/nature" alt="galeria" />
